3939f1676e3617c3e412fef2c7b5d6276c3d3060
soywiz <soywiz@gmail.com>
2023-10-04 21:55:18 +0200

    Allow updating korge version via gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,35 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 val gitSubmodules: List<File> by lazy {
     (rootDir.listFiles()?.toList() ?: emptyList()).filter {
         it.isDirectory && File(it, ".git").isFile
     }
 }
 
-fun File.git(vararg args: String, fail: Boolean = true) {
-    println("git ${args.joinToString(" ")}")
+fun File.isNewTemplate(): Boolean {
+    val file = File(this, "settings.gradle.kts").takeIf { it.exists() } ?: return false
+    return "com.soywiz.korge.settings:com.soywiz.korge.settings.gradle.plugin" in file.readText()
+}
+
+fun File.exec(vararg commandline: String, fail: Boolean = true, cmd: Boolean = true) {
+    println("${commandline.joinToString(" ")}")
     try {
         exec {
-            workingDir(this@git)
-            commandLine("git", *args)
+            workingDir(this@exec)
+            commandLine(*buildList {
+                if (cmd && Os.isFamily(Os.FAMILY_WINDOWS)) {
+                    add("cmd.exe")
+                    add("/c")
+                }
+                addAll(commandline)
+            }.toTypedArray())
         }
     } catch (e: Throwable) {
         if (fail) throw e
     }
 }
+
+fun File.git(vararg args: String, fail: Boolean = true) = exec("git", *args, fail = fail)
 
 tasks {
     val updateRepos by creating {
@@ -62,6 +77,70 @@ tasks {
                     file.git("commit", "-m", "Make gradlew executable", fail = false)
                     file.git("push")
                     println("DONE")
+                } else {
+                    println("DO NOT EXISTS")
+                }
+            }
+        }
+    }
+    val updateKorgeVersion by creating {
+        doLast {
+            for (file in gitSubmodules) {
+                print("FILE: $file...")
+
+                val libsVersionsToml = File(file, "gradle/libs.versions.toml")
+                val yarnLockFile = File(file, "kotlin-js-store/yarn.lock")
+
+                if (!libsVersionsToml.isFile) {
+                    println("Can't find $libsVersionsToml")
+                    continue
+                }
+                if (!file.isNewTemplate()) {
+                    println("Not using the new template")
+                    continue
+                }
+
+                val newKorgeVersion = "5.0.4"
+                val libsVersionsTomlText = libsVersionsToml.readText()
+                val regeex = Regex("^korge = \\{ id = \"com.soywiz.korge\", version = \"(.*?)\" \\}", RegexOption.MULTILINE)
+                val regeex2 = Regex("^#korge = \\{ id = \"com.soywiz.korge\", version = \"(.*?)\" \\}", RegexOption.MULTILINE)
+
+                val updatedText = libsVersionsTomlText
+                    .replace(regeex, "korge = { id = \"com.soywiz.korge\", version = \"$newKorgeVersion\" }")
+                    .replace(regeex2, "#korge = { id = \"com.soywiz.korge\", version = \"999.0.0.999\" }")
+
+                if (libsVersionsTomlText != updatedText) {
+                    libsVersionsToml.writeText(updatedText)
+
+                    if (yarnLockFile.isFile) {
+                        file.exec("gradlew", "kotlinUpgradeYarnLock")
+                    }
+
+                    file.git("add", "gradle/libs.versions.toml")
+                    if (yarnLockFile.isFile) file.git("add", "kotlin-js-store/yarn.lock")
+                    file.git("commit", "-m", "Upgrade KorGE to $newKorgeVersion", fail = false)
+                    file.git("push")
+                    println("UPDATED")
+                } else {
+                    println("ALREADY UPDATED")
+                }
+
+            }
+        }
+    }
+    val upgradeYarnLockJs by creating {
+        doLast {
+            for (file in gitSubmodules) {
+                print("FILE: $file...")
+
+                if (File(file, "gradlew").isFile) {
+                    try {
+                        file.exec("gradlew", "kotlinUpgradeYarnLock")
+                        println("DONE")
+                    } catch (e: Throwable) {
+                        e.printStackTrace()
+                        println("ERROR")
+                    }
                 } else {
                     println("DO NOT EXISTS")
                 }
